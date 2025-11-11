825a0d80c3401ae6a3af6979a245e22662bb6716
Rufus Christian <christian.rufus23@gmail.com>
2025-11-11 00:51:42 +0000

    Create file to convert hours to seconds

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+#define length and width variables for the rectangle
+length = 10
+width = 5
+
+#calculate the area of the rectangle using the formula Area = length x width
+area = length * width
+
+#print the calculated area to the console
+print("The area of the rectangle is:", area)
