691768b62bce14fd90d89b4971ec5eaff63e660b
Rufus Christian <christian.rufus23@gmail.com>
2025-11-11 00:17:56 +0000

    Perform basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#define the two numbers
+number1 = 10
+number2 = 5
+
+#perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+#print the results of each operation
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
