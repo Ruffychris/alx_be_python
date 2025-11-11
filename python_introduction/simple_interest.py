d1073b707cd8cef3d35c9375cd92272c6495a995
Rufus Christian <christian.rufus23@gmail.com>
2025-11-11 00:34:57 +0000

    Calculate the simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#define variables to be used to calculate the simple interest
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculate the simple interest with the formula I = P*R*T
+interest = principal * rate * time
+
+#print the calculated interest
+print("The simple interest is", interest)
