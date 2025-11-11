6d4a76f1629dab59921861fb25bcd67e0076d00a
Rufus Christian <christian.rufus23@gmail.com>
2025-11-11 01:18:08 +0000

    User Input age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+#prompt user to input their age
+current_age = int(input("How old are you?"))
+
+#calculate user's future age
+future_age = current_age + 27
+
+#print user's age in 2050
+print("In 2050, you will be", future_age, "years old.")
